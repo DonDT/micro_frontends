cbbdd3de41f21c434f1ea74448244cedae863822
Donald <ediagee@yahoo.com>
2021-08-23 12:32:23 +0300

    Testing s3 bucket setup

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -4,6 +4,8 @@ import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+///
+
 export default () => {
   return (
     <div>
